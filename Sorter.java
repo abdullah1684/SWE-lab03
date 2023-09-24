179355b72e179c7089b17cdc1bfa887c39d56ce4
AHMED ALHARBI <ahmadalharbi99@gmail.com>
2023-09-24 21:06:13 +0300

    Add insertionSort and selectionSort methods

---
+++ Sorter.java
@@ -0,0 +1,44 @@
+    class Sorter{
+       
+    public static void main(String[] args) {
+        int[] arr = {88, 4, 9, 500, 365};
+        selectionSort(arr);
+        for(int i=0; i<5;i++){
+
+            
+            System.out.println(arr[i]);
+        }
+
+    }
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
